265ac2c037cb1cece95ff1a898621cdace3851fd
Renaldy <renalfrontend@gmail.com>
2021-02-24 01:12:55 +0700

    Header Complete

---
+++ src/components/Header/Header.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import style from './header.module.css';
+import Typed from 'react-typed';
+import Particles from 'react-particles-js';
+
+function Headers(){
+   return(
+      <div className={style.header}>
+         <div className={style.headerInfo}>
+            <h1>web development and website promotions</h1>
+            <Typed
+               className={style.typedText}
+               strings={['Web Design','Web Development','Google Ads']}
+               typeSpeed={60}
+               backSpeed={30}
+               loop
+            />
+            <div className={style.containerContact}>
+               <a href="#" className={style.btnContact}>Contact me</a>
+            </div>
+         </div>
+
+         <Particles
+            params={{
+               particles : {
+                  number : {
+                     value : 30,
+                     density : {
+                        enable : true,
+                        value_area : 900
+                     }
+                  },
+                  shape : {
+                     type : 'square',
+                     stroke : {
+                        width : 6,
+                        color : '#f9ab00'
+                     }
+                  }
+               }
+            }}
+            className={style.particle}
+         />
+      </div>
+   )
+}
+
+export default Headers;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './global.css';
 import Navbars from './components/Navbar/Navbar';
+import Headers from './components/Header/Header';
 
 ReactDOM.render(
   <React.StrictMode>
     <Navbars/>
+    <Headers/>
   </React.StrictMode>,
   document.getElementById('root')
 );
